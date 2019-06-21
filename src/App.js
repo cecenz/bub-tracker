860f6f75708fe6c30152c78f4fa38f0ea23af98b
cece <claire.campbellnz@gmail.com>
2019-06-22 09:03:51 +1200

    adding animation for cards

--- src/App.js
+++ src/App.js
@@ -1,29 +1,74 @@
-import React from "react";
-import ActivityAdder from "./ActivityAdder";
-import { ThemeProvider } from "styled-components";
+import React, { Fragment, useState } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+// import ActivityAdder from "./ActivityAdder";
 import { yellow, blue, green, purple, pink } from "./themes";
+import Card from "./Card";
+import styled from "styled-components";
+import { ReactComponent as NextIcon } from "./svg/next.svg";
+import { ReactComponent as BackIcon } from "./svg/back.svg";
+import { ReactComponent as PlusIcon } from "./svg/plus.svg";
 
 import "./globalStyles.css";
 
 function App() {
+  const [isShowCards, setShowCards] = useState(false);
+
+  const onClick = () => setShowCards(!isShowCards);
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <Fragment>
+      <Header>
+        <PlusIcon
+          onClick={() => onClick()}
+          style={{ width: "16px", height: "16px" }}
+        />
+        <TransitionGroup component={null}>
+          {isShowCards && (
+            <CSSTransition classNames="dialog" timeout={750}>
+              <ContentContainer>
+                <Card title="Nappy" theme={blue} />
+                <Card title="Feed" theme={yellow} />
+                <Card title="Activity" theme={green} />
+                <Card title="Sleep" theme={purple} />
+                <Card title="Media" theme={pink} />
+              </ContentContainer>
+            </CSSTransition>
+          )}
+        </TransitionGroup>
+      </Header>
+      <DashboardHeader>
+        <BackIcon style={{ width: "16px", height: "16px" }} />
         <h1>Today</h1>
-      </header>
-      <div>
+        <NextIcon style={{ width: "16px", height: "16px" }} />
+      </DashboardHeader>
+
+      {/* <div>
         <ThemeProvider theme={yellow}>
           <ActivityAdder />
         </ThemeProvider>
-      </div>
-      {/* <Card theme={yellow} />
-      
-      <Card theme={blue} />
-      <Card theme={green} />
-      <Card theme={purple} />
-      <Card theme={pink} />       */}
-    </div>
+      </div> */}
+    </Fragment>
   );
 }
+
+const ContentContainer = styled.div`
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 1rem;
+`;
+
+const Header = styled.div`
+  padding: 1rem;
+  background-color: #fff;
+  box-shadow: 0 2px 12px 0 #eee;
+`;
+
+const DashboardHeader = styled.div`
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -6,11 +6,11 @@ const StyledCard = styled.div`
   padding: 1rem;
   border-radius: 4px;
   box-shadow: 0 2px 8px 0 #ccc;
-  margin: 2rem 0;
+  margin: 1rem 0;
   cursor: pointer;
   transition: all 0.2s ease-in;
   :hover {
-    box-shadow: 2px 6px 15px 2px #ccc;
+    box-shadow: 0px 4px 8px 0px #ccc;
     background-color: ${props =>
       props.theme.highlight && props.theme.highlight};
     transition: all 0.2s ease-in;
@@ -24,15 +24,15 @@ StyledCard.defaultProps = {
   }
 };
 
-const Card = ({ theme }) => {
+const Card = ({ theme, title = "foobar" }) => {
   return (
     <ThemeProvider theme={theme}>
       <StyledCard>
-        <h2>Heading</h2>
+        <h2>{title}</h2>
         <p>Here is a bunch of content for the button</p>
       </StyledCard>
     </ThemeProvider>
   );
 };
 
-export default Card;    
+export default Card;
